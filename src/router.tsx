ca40945c39630258760d813dc9f79640d74f4ba8
arivuwallet-sketch <arivuwallet@gmail.com>
2026-09-19 20:46:55 +0530

    refactor: reuse shared CSP nonce accessor

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,8 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
-import { createIsomorphicFn, getGlobalStartContext } from "@tanstack/react-start";
 import { routeTree } from "./routeTree.gen";
 
-const getCspNonce = createIsomorphicFn()
-  .server(() => getGlobalStartContext()?.cspNonce as string | undefined)
-  .client(
-    () => document.querySelector('meta[property="csp-nonce"]')?.getAttribute("content") ?? undefined,
-  );
+import { getCspNonce } from "./lib/csp";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
